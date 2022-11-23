287ee0ec36fdf2256eb29e08662ff0da8443d94e
dev-huiya <k_hawon@naver.com>
2022-11-23 19:42:38 +0900

    비밀번호 목록 호출 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
 </template>
 
 <style lang="less">
-@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700&display=swap');
 
 * {
@@ -24,6 +23,8 @@ html, body {
   --text-color: #333;
   // 텍스트 반전 색상 (배경 진할때 용)
   --text-invert-color: #FFF;
+  // 옅은 텍스트 색상
+  --text-lighten-color: #777;
   // 메인 컬러
   --primary-color: #655cb7;
   // 메인 두번째 컬러
@@ -58,6 +59,8 @@ html, body {
     --text-color: #d7d7d7;
     // 텍스트 반전 색상 (배경 진할때 용)
     --text-invert-color: #FFF;
+    // 옅은 텍스트 색상
+    --text-lighten-color: #959595;
     // 메인 컬러
     --primary-color: #655cb7;
     // 메인 두번째 컬러
@@ -102,6 +105,22 @@ body {
 }
 .mosha__toast__content__text {
   font-weight: normal;
+}
+
+// project custom style
+.page-container {
+  padding-top: 50px;
+  padding-bottom: 100px;
+}
+.link-with-icon {
+  display: inline-flex;
+  align-items: center;
+  gap: 10px;
+}
+.box {
+  box-shadow: var(--color-shadow-medium);
+  background-color: var(--background-primary-color);
+  border-radius: 7px;
 }
 </style>
 <script>
---
+++ src/views/parts/ListBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-link :to="`/password/${data.host}`" class="box-list">
+    <span>{{ `${data.protocol}://${data.host}` }}</span>
+    <arrow-right-icon width="20" height="20" />
+  </router-link>
+</template>
+
+<script>
+import ArrowRightIcon from '@/assets/feather-icons/ArrowRight'
+export default {
+  name: 'list-box',
+  components: { ArrowRightIcon },
+  props: {
+    data: {
+      type: Object,
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
